e974c5496a361cefa765bb0da70b60a711c7554e
“starwavelin” <benjaminxianlin@gmail.com>
2024-08-19 20:01:52 -0400

    chore: LC 3 sol 2 up

--- string/sliding_window/LongestSubstringWithoutRepeatingCharactersSol2.java
+++ string/sliding_window/LongestSubstringWithoutRepeatingCharactersSol2.java
@@ -4,44 +4,44 @@
  * Date        : August 19, 2024
  * Author      : @codingbro
  *
- * meta        : tag-sliding-window, tag-substring, tag-set
+ * meta        : tag-sliding-window, tag-substring, tag-map
  ***************************************************************************/
 
 package string.sliding_window;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
-public class LongestSubstringWithoutRepeatingCharacters {
+public class LongestSubstringWithoutRepeatingCharactersSol2 {
 
     public int lengthOfLongestSubstring(String s) {
         int n = s.length();
 
-        int len = 0; // store the result
-        Set<Character> set = new HashSet<>(); // store the characters from s
+        int len = 0; // store the max length
+        Map<Character, Integer> map = new HashMap<>(); // key - char, val - count
 
         for (int l = 0, r = 0; r < n; r++) {
-            // handle r - part 1 is r++
+            // handle r
+            char cur = s.charAt(r);
+            map.put(cur, map.getOrDefault(cur, 0) + 1);
 
-
-            // handle l, after the end of this while loop, l will be at the new starting position of a possible longest
-            // substring
-            while (set.contains(s.charAt(r))) {
-                set.remove(s.charAt(l));
+            // handle l, when repetition of cur happens, move l
+            while (map.get(cur) == 2) {
+                char left = s.charAt(l);
+                map.put(left, map.get(left) - 1);
                 l++;
             }
 
-            set.add(s.charAt(r)); // handle r - part 2
-
-            // update max result
+            // result, max result
             len = Math.max(len, r - l + 1);
         }
 
         return len;
     }
 
+
     public static void main(String[] args) {
-        LongestSubstringWithoutRepeatingCharacters test = new LongestSubstringWithoutRepeatingCharacters();
+        LongestSubstringWithoutRepeatingCharactersSol2 test = new LongestSubstringWithoutRepeatingCharactersSol2();
         String s1 = "abcbdeca";
         System.out.println(test.lengthOfLongestSubstring(s1)); // 5
 
